d7c9b5c2e6010a6d774e0519b0903f11c6dee453
Oscar Cerna-Mandujano <oscar.cerna@outlook.com>
2018-02-12 23:20:57 -0800

    Added a helper function to read the Windows registry.

--- DBFZ-mod-manager/Helper.cs
+++ DBFZ-mod-manager/Helper.cs
@@ -16,5 +16,35 @@ namespace ModManager
                 "<.*?>", String.Empty
             );
         }
+
+        /// <summary>
+        /// Attempts to read Window's regestry value's data. Returns false if unsuccessfull.
+        /// </summary>
+        /// <param name="KeyName">Key with HKEY_LOCAL_MACHINE as the base address.</param>
+        /// <param name="ValueName">Address value</param>
+        /// <param name="data">Address value data</param>
+        /// <returns></returns>
+        public static bool GetWindowsRegestryValueData(string KeyName, string ValueName, out string data)
+        {
+            data = null;
+            try
+            {
+                using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(KeyName))
+                {
+                    if (key != null)
+                    {
+                        Object o = key.GetValue(ValueName);
+                        if (o != null)
+                        {
+                            data = (string)o;
+                            return true;
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            return false;
+        }
     }
 }
