d2a4f192cf717d579e868560b85e1954726fecc2
Oscar Cerna-Mandujano <oscar.cerna@outlook.com>
2018-02-12 23:36:14 -0800

    Added a new setting which holds the registry key for Steam (which is a 32-bit program) in a 64-bit version of Windows. It is needed in order to find the install path of DragonBall FighterZ on the user's computer.

--- DBFZ-mod-manager/Properties/Settings.Designer.cs
+++ DBFZ-mod-manager/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ModManager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,18 @@ namespace ModManager.Properties {
             }
             set {
                 this["gamePath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("SOFTWARE\\WOW6432Node\\Valve\\Steam")]
+        public string SteamWindows64BitRegistryKey {
+            get {
+                return ((string)(this["SteamWindows64BitRegistryKey"]));
+            }
+            set {
+                this["SteamWindows64BitRegistryKey"] = value;
             }
         }
     }
